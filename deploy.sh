36465fffab51709cb36a036e61e093b441aa7dc3
Osamu Yamakami <yamakami_osamu@liginc.co.jp>
2024-01-13 11:09:17 +0900

    Update to foucault_web=0.1-00012

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-function sample() {
-  echo "start to deploy foucault-web";
-}
+echo "start to deploy foucault-web";
 
 function increment_version() {
-    version_str=$(cat .version)
-    prefix=$(echo $version_str | sed 's/-.*//')
-    number_part=$(echo $version_str | sed 's/.*-//')
-    new_number_part=$(printf "%05d" $((10#$number_part + 1)))
-    NEW_VERSION_STR="${prefix}-${new_number_part}"
+  version_str=$(cat .version)
+  prefix=$(echo $version_str | sed 's/-.*//')
+  number_part=$(echo $version_str | sed 's/.*-//')
+  new_number_part=$(printf "%05d" $((10#$number_part + 1)))
+  NEW_VERSION_STR="${prefix}-${new_number_part}"
 
-    echo $NEW_VERSION_STR > .version
-    echo $NEW_VERSION_STR
+  echo $NEW_VERSION_STR > .version
+  echo $NEW_VERSION_STR
 }
 
 # 関数を呼び出して新しいバージョンを取得
@@ -25,9 +23,9 @@ git commit -m "Update to $NEW_VERSION_STR";
 
 # 引数があればそのブランチにpush、なければmainブランチにpush
 if [ -z "$1" ]; then
-    git push;
+  git push;
 else
-    git push origin $1;
+  git push origin $1;
 fi
 
 date "+%Y-%m-%d %a %H:%M:%S";
