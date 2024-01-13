6871daff125a8e1ca63eb6f2af24b91c5709c6d0
Osamu Yamakami <yamakami_osamu@liginc.co.jp>
2024-01-13 11:04:38 +0900

    Update to foucault_web=0.1-00010

--- deploy.sh
+++ deploy.sh
@@ -1,46 +1,28 @@
 #!/bin/bash
 
-
-
-
-
 function sample() {
   echo "foobar";
 }
 
 function increment_version() {
-    # .version ファイルから現在のバージョンを読み込む
     version_str=$(cat .version)
-
-    # ハイフンより前の文字列を保持（例: foucault-web=0.01）
     prefix=$(echo $version_str | sed 's/-.*//')
-
-    # ハイフン以降の数字部分を取得（例: 00001）
     number_part=$(echo $version_str | sed 's/.*-//')
-
-    # 数字部分をカウントアップ
     new_number_part=$(printf "%05d" $((10#$number_part + 1)))
+    NEW_VERSION_STR="${prefix}-${new_number_part}"
 
-    # 新しいバージョン番号を構築
-    new_version_str="${prefix}-${new_number_part}"
-
-    # .version ファイルに新しいバージョンを書き込む
-    echo $new_version_str > .version
-
-    # 新しいバージョン番号を出力
-    echo $new_version_str
+    echo $NEW_VERSION_STR > .version
+    echo $NEW_VERSION_STR
 }
 
-# 関数を呼び出す
+# 関数を呼び出して新しいバージョンを取得
 increment_version
 
 # gitの操作
-git status;
 git add -A;
-git commit -m "Update to $GLOBAL_NEW_VERSION_STR";
+git status;
+git commit -m "Update to $NEW_VERSION_STR";
 git push;
 
 date "+%Y-%m-%d %a %H:%M:%S";
-echo $new_version_str;
-
-
+echo $NEW_VERSION_STR;
